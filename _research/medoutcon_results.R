b491d60ec9f7161a5ef68f6159aecc760452867b
nt-williams <ntwilliams.personal@gmail.com>
2023-10-24 11:12:19 -0700

    Updated medoutcon sim

--- _research/medoutcon_results.R
+++ _research/medoutcon_results.R
@@ -5,70 +5,39 @@ source("_research/dcmeSim.R")
 
 devtools::source_gist("https://gist.github.com/nt-williams/3afb56f503c7f98077722baf9c7eb644")
 
-truth <- dcmeSim$new(5e6)$truth()
+IIE <- 0.02314648
+IDE <- 0.1387395
+bound_IDE <- 0.7008895
+bound_IIE <- 0.0867789
 
 ns <- c(500, 1000, 5000, 1e4)
 tars <- glue("_research/data/moc_{ns}.zip")
 
-res <- map_dfr(tars, ~ map_dfr(.x, read_zip_rds, .id = "i"), .id = "n") |> 
-    select(n, i, est = param_est, conf.low = lwr_ci, conf.high = upr_ci, param) |> 
-    mutate(param = gsub("_interventional", "", param)) |> 
-    pivot_wider(names_from = param, 
-                values_from = est:conf.high, 
-                id_cols = c(i, n)) |> 
-    mutate(n = case_when(n == 1 ~ 500, 
-                         n == 2 ~ 1000, 
-                         n == 3 ~ 5000, 
+res <- map_dfr(tars, ~ map_dfr(.x, read_zip_rds, .id = "i"), .id = "n") |>
+    select(n, i, est = param_est, conf.low = lwr_ci, conf.high = upr_ci, param) |>
+    mutate(param = gsub("_natural", "", param)) |>
+    pivot_wider(names_from = param,
+                values_from = est:conf.high,
+                id_cols = c(i, n)) |>
+    mutate(n = case_when(n == 1 ~ 500,
+                         n == 2 ~ 1000,
+                         n == 3 ~ 5000,
                          n == 4 ~ 1e4))
-    
 
 summary <- function(x) {
     group_by(x, n) |>
         filter(between(est_indirect, -1, 1),
                between(est_direct, -1, 1)) |>
-        summarize(indirect_bias = abs(mean(est_indirect - truth$CIIE)),
-                  direct_bias = abs(mean(est_direct - truth$CIDE)),
-                  indirect_covr = mean(map2_lgl(conf.low_indirect, conf.high_indirect, ~ between(truth$CIIE, .x, .y))),
-                  direct_covr = mean(map2_lgl(conf.low_direct, conf.high_direct, ~ between(truth$CIDE, .x, .y))),
-                  indirect_relmse = mean(n * (est_indirect - truth$CIIE)^2 / truth$bound_CIIE),
-                  direct_relmse = mean(n * (est_direct - truth$CIDE)^2 / truth$bound_CIDE)) |>
+        summarize(indirect_bias = abs(mean(est_indirect - IIE)),
+                  direct_bias = abs(mean(est_direct - IDE)),
+                  indirect_covr = mean(map2_lgl(conf.low_indirect, conf.high_indirect, ~ between(IIE, .x, .y))),
+                  direct_covr = mean(map2_lgl(conf.low_direct, conf.high_direct, ~ between(IDE, .x, .y))),
+                  indirect_relmse = mean(n * (est_indirect - IIE)^2 / bound_IIE),
+                  direct_relmse = mean(n * (est_direct - IDE)^2 / bound_IDE)) |>
         mutate(across(c(indirect_bias, direct_bias), ~ .x * sqrt(n), .names = "{.col}rootn"))
 }
 
-summary(res) |> 
-    pivot_longer(indirect_bias:direct_biasrootn, 
-                 names_to = c("param", ".value"), names_pattern = "(.+)_(.+)")
-
-make_table <- function(data) {
-    kableExtra::kbl(data,
-                    "latex",
-                    booktabs = TRUE,
-                    digits = 3,
-                    linesep = "",
-                    col.names = c("Misspecifed", "$n$", "$\\text{Bias}$", 
-                                  "$\\sqrt{n} \\times \\text{Bias}$", 
-                                  "95\\% CI Covr.", "Rel. Efficiency"),
-                    escape = FALSE,
-                    align = "clcccc") |>
-        kableExtra::collapse_rows(1, "middle")
-}
-
-imap_dfr(c(500, 1000, 5000, 1e4), function(n, i) {
-    map_dfr(res[[i]], function(x) {
-        data.frame(n = n, 
-                   cide = x$CIDE, 
-                   ciie = x$CIIE,
-                   cide.conf.low = ci(x$CIDE, x$D_CIDE, n)[1], 
-                   cide.conf.high = ci(x$CIDE, x$D_CIDE, n)[2],
-                   ciie.conf.low = ci(x$CIIE, x$D_CIIE, n)[1], 
-                   ciie.conf.high = ci(x$CIIE, x$D_CIIE, n)[2])
-    })
-}) |> 
-    summary() |> 
-    pivot_longer(ciie_bias:cide_biasrootn, 
-                 names_to = c("param", ".value"), names_pattern = "(.+)_(.+)") |> 
-    mutate(misspec = spec, .before = n) |> 
-    select(misspec, n, param, bias, biasrootn, everything()) |> 
-    arrange(param) |> 
-    select(-param) |> 
-    make_table()
+summary(res) |>
+    pivot_longer(indirect_bias:direct_biasrootn,
+                 names_to = c("param", ".value"), names_pattern = "(.+)_(.+)") |>
+  make_table()
--- _research/medoutcon_sim.R
+++ _research/medoutcon_sim.R
@@ -15,20 +15,19 @@ glm_lrnr <- Lrnr_glm$new()
 lgb_lrnr <- Lrnr_lightgbm$new()
 
 lrnr_lib <- Stack$new(mean_lrnr, glm_lrnr, lgb_lrnr)
-sl_lrnr <- Lrnr_sl$new(learners = lrnr_lib, metalearner = Lrnr_nnls$new(), 
+sl_lrnr <- Lrnr_sl$new(learners = lrnr_lib, metalearner = Lrnr_nnls$new(),
                        cv_control = list(V = 5))
 
 simulate <- function(n) {
     data <- dcmeSim$new(n)$data
     folds <- ifelse(n >= 1e4, 5, 10)
-    
-    W <- data[, paste0("W", 1:3)]
-    A <- data$A
-    Z <- data$Z
+
+    W <- data[, c(paste0("W", 1:3))]
+    A <- data$Z
     M <- data$M
     Y <- data$Y
-    
-    de <- medoutcon(W, A, Z, M, Y,
+
+    de <- medoutcon(W, A, NULL, M, Y,
                    effect = "direct",
                    contrast = NULL,
                    g_learners = sl_lrnr,
@@ -40,8 +39,8 @@ simulate <- function(n) {
                    v_learners = sl_lrnr,
                    estimator = "onestep",
                    estimator_args = list(cv_folds = 2))
-    
-    ide <- medoutcon(W, A, Z, M, Y,
+
+    ide <- medoutcon(W, A, NULL, M, Y,
                      effect = "indirect",
                      contrast = NULL,
                      g_learners = sl_lrnr,
@@ -53,11 +52,11 @@ simulate <- function(n) {
                      v_learners = sl_lrnr,
                      estimator = "onestep",
                      estimator_args = list(cv_folds = 2))
-    
+
     bind_rows(summary(de), summary(ide))
 }
 
-saveRDS(simulate(args[1]), 
+saveRDS(simulate(args[1]),
         glue::glue("_research/data/moc_{args[1]}_{id}.rds"))
 
 quit("no")
